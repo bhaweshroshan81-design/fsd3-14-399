2f531a2e5001ab18502b63d6f777fb0355d5fc10
bhawesh roushan <bhaweshroshan81@gmail.com>
2026-09-17 13:59:14 +0530

    basic promting with express server

--- lab5/app.js
+++ lab5/app.js
@@ -3,8 +3,29 @@ import express from "express";
 const app =express();
 
 app.get ("/", (req,res)=>{
-    res.send("<h1>Hello World");
+    res.send("<h1>Hello Express");
 });
-app.listen(3000, ()=>{
-    console.log("Server is running on port 3000");
+
+app.get('/about', (req,res)=>{
+    res.send("We are FSD developers");
+});
+
+app.post('/login', (req,res)=>{
+    res.send({msg:'user login'});
+});
+
+app.put('/user/update', (req,res)=>{
+    res.send({msg:'user update'});
+});
+
+app.delete('/user/delete', (req,res)=>{
+    res.send({msg:'user delete'});
+});
+
+app.use((req,res)=>{
+    res.status(404).send("<h1>Page Not Found</h1>");
+});
+
+app.listen(3333, ()=>{
+    console.log("Server is running on port 3333");
 });
